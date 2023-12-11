dc6d3c635f6f967f09c53bde839650809509c2e3
Alexandre Lima <alexandrelima.beta@gmail.com>
2023-12-11 12:45:22 -0300

    theme created

---
+++ src/styles/theme.js
@@ -0,0 +1,33 @@
+const theme = {
+   color: {
+      light: '#FFFFFF',
+      dark: '#3F403F',
+      primary: '#008170',
+   },
+   mensure: {
+      half: '0.5rem',
+      single: '1rem',
+      double: '2rem',
+      triple: '3rem',
+   },
+   size: {
+      small: '1rem',
+      regular: '1.625rem',
+      medium: '3.125rem',
+      large: '5.625rem',
+      extra: '7.5rem',
+   },
+   weight: {
+      small: 200,
+      regular: 400,
+   },
+   height: {
+      small: '1.3125rem',
+      regular: '1.9375rem',
+      medium: '3.4375rem',
+      large: '5.9375rem',
+      extra: '7.8125rem',
+   },
+};
+
+export default theme;
